cbe22c51778a3316ab058e7d3a38147d7bade1e8
unknown <r.kevensantos7@gmail.com>
2024-09-22 01:16:44 -0300

    resgate de pensamentos de usuários espec.

--- controller/toughts.js
+++ controller/toughts.js
@@ -1,13 +1,51 @@
 const Toughts = require('../models/toughts')
+const User = require('../models/auth')
 
 module.exports = class ToughtsController{
 
     static async Showall(req, res){
+
         res.render('toughts/home')
     }
     
-    static dashboard(req, res){
-        res.render('toughts/dashboard')
+    static async dashboard(req, res){
+
+        const UserId = req.session.userid
+
+        const user = await User.findOne({where: {id : UserId},
+            include: Toughts,
+            plain: true
+        })
+
+        const toughts = user.Toughts.map((result)=>result.dataValues)
+
+        console.log(user)
+        
+        res.render('toughts/dashboard', {toughts})
+    }
+
+    static Create(req, res){
+        res.render('toughts/create')
+    }
+
+    static async CreateSave(req, res){
+
+        const tougth = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try{
+            await Toughts.create(tougth)
+            req.flash('message', 'PENSAMENTO CRIADO COM SUCESSO')
+            req.session.save(()=>{
+                res.redirect('/toughts/dashboard')
+            })
+        }catch(err){
+            console.log(err)
+        }
+
+
     }
 
 }
--- router/toughts.js
+++ router/toughts.js
@@ -6,5 +6,7 @@ const CheckUser = require('../helpers/authHelp').CheckUser
 
 router.get('/', Toughts.Showall)
 router.get('/dashboard', CheckUser, Toughts.dashboard)
+router.get('/create', CheckUser, Toughts.Create)
+router.post('/create', CheckUser, Toughts.CreateSave)
 
 module.exports = router
